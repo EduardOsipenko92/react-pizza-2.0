c64e9eb1252caf22316405d16c76252506f43f58
EduardOsipenko92 <114285639+EduardOsipenko92@users.noreply.github.com>
2024-07-02 23:09:29 +0300

    first commit

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { SearchContext } from "../../App";
+import styles from "./Search.module.scss";
+import debounce from "lodash.debounce";
+
+const Search = () => {
+  const [value, setValue] = React.useState("");
+  const { setSearchValue } = React.useContext(SearchContext);
+  const inputRef = React.useRef();
+
+  const onClickClear = () => {
+    setSearchValue("");
+    setValue("");
+    inputRef.current.focus();
+  };
+
+  const updateSearchValue = React.useCallback(
+    debounce((str) => {
+      setSearchValue(str);
+    }, 1000),
+    []
+  );
+
+  const onChangeInput = (e) => {
+    setValue(e.target.value);
+    updateSearchValue(e.target.value);
+  };
+
+  return (
+    <div className={styles.root}>
+      <svg
+        className={styles.icon}
+        fill="none"
+        height="24"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        viewBox="0 0 24 24"
+        width="24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="11" cy="11" r="8" />
+        <line x1="21" x2="16.65" y1="21" y2="16.65" />
+      </svg>
+      <input
+        ref={inputRef}
+        value={value}
+        onChange={onChangeInput}
+        className={styles.input}
+        placeholder="Поиск пиццы..."
+      />
+      {value && (
+        <svg
+          onClick={onClickClear}
+          className={styles.clearIcon}
+          viewBox="0 0 48 48"
+          xmlns="http://www.w3.org/2000/svg"
+          opacity="0.3"
+        >
+          <path d="M38 12.83l-2.83-2.83-11.17 11.17-11.17-11.17-2.83 2.83 11.17 11.17-11.17 11.17 2.83 2.83 11.17-11.17 11.17 11.17 2.83-2.83-11.17-11.17z" />
+          <path d="M0 0h48v48h-48z" fill="none" />
+        </svg>
+      )}
+    </div>
+  );
+};
+
+export default Search;
